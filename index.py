3f094aa4dccc7129555d3a4d25f6b38a0eeb3516
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2022-10-06 11:16:11 -0300

    version 0.01
    Descrição

---
+++ index.py
@@ -0,0 +1,28 @@
+##------------------ Notebook do R
+# Extensão *.Rmd
+# Abre automaticamente no RStudio
+# Arquivos de Dados devem estar no mesmo diretório do arquivo *.Rmd
+
+
+##--------------- Notebook do R
+#Código é divido em blocos
+#Você pode rodar a linha a linha (ctrl + enter)
+#Você pode rodar todo o bloco (Ctrl + shift + Enter)
+
+# Em alguns casos você deve executar todo o bloco (Ctrl + Shift + Enter)
+
+
+##----------------
+#Local -> Ambiente: ANACONDA   IDE: JUPYTER  -> Anaconda + RStudio
+#Nuvem -> Ambiente: Google Colab IDE: JUPYTER    -> https://colab.research.google.com
+
+# O que é um google Colab? É um ambiente 100% cloud, gratuito, CPU OU GPU gratuítu (sem garantia) com tempo máximo de execução 12 horas. Contém suas principais bibliotecas já instaladas. Armazena notebooks no Google Drive e Colab Pro: UD$9.99 mês
+
+
+##----------DICAS ADICIONAIS 
+
+##
+# LOCAL: Para adicionar biblioteca: !pip install[nomedabibliote]
+# CLOUD: Você precisa fazer upload dos dados: from google.colab import files upload = files.upload()
+
+
