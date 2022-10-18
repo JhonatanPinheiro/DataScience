d8a6c67e32bebd35cc667fe746c426ee0bdda8be
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2022-10-18 14:11:46 -0300

    version_001.py

---
+++ jupyter_panda.py
@@ -0,0 +1,10 @@
+#Fazendo a importação da biblioteca panda (Usando o Jypter)
+import pandas as pd
+
+
+#Carrega arquivo para dataframe Pandas
+dados  =  pd.read_cvs("data_py/credit.csv")
+#Formato
+dados.shape
+
+
